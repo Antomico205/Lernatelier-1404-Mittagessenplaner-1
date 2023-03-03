1d439d2c6dde74ed66b517a6a6e9b5537c63260b
Antonio <antonio.neuweiler@bluewin.ch>
2023-03-03 07:48:29 +0100

    Test

--- Website/Anmeldedaten/daten.js
+++ Website/Anmeldedaten/daten.js
@@ -1,1 +1,48 @@
-gaga
+module.exports = {
+    InitialData: [
+        
+        {
+            login: 1,
+            name: "Samuel Brunner",
+            passwort: 12345
+        },
+
+        {
+            login: 2,
+            name: "Tiago Friedli",
+            passwort: 098765
+        },
+
+        {
+            login: 3,
+            name: "Nikola Curaz",
+            passwort: 19283
+        },
+
+        {
+            login: 4,
+            name: "Thierry Gambon",
+            passwort: 123678
+        },
+
+        {
+            login: 5, 
+            name: "Loris Konetzny",
+            passwort: 192837
+        },
+
+        {
+            login: 6,
+            name: "Jeremy Bärlocher",
+            passwort: 543210
+        },
+
+        {
+            login: 7,
+            name: "Lukas Bargiesen",
+            passwort: 9876543
+        }
+
+
+    ]
+    }   ;
